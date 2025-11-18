ec875dc50e6c2913dd0ed56f35acbffa282dbc38
zawnia <al84250@gmail.com>
2025-11-18 10:00:56 +0100

    correcting folder's name

---
+++ TP6-7/personnelAdmin.cpp
@@ -0,0 +1,8 @@
+#include "personnelAdmin.h"
+#include <iostream>
+
+PersonnelAdmin::PersonnelAdmin(const std::string &nom, const std::string &prenom, const std::string &adresse, const std::string &telephone) : Personnel(nom, prenom, adresse, telephone)
+{
+    
+}
+
